2e4ffe1c204a5c55cca491f63066d0e7897c8378
SmallUpdate <112966801+SmallUpdate@users.noreply.github.com>
2022-10-17 20:01:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ $(function() {
   $(".button").click(function(){
     $('header').toggleClass("header_show")
   });
-
+  
+  $(".menu_item").click(function(){
+    $('header').toggleClass("header_show")
+  });
 });
